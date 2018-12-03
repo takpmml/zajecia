7fcb4e97fcceb32c8e76c0b0997c8c04ef2ac616
marcin.mlotkowski <marcin.mlotkowski@testuj.pl>
2018-12-03 11:21:51 +0100

    Założyłem repo i dodałem plik z rozmienianiem monet

---
+++ rozmienianie.py
@@ -0,0 +1,30 @@
+def rozmien(portmonetka, kwota):
+    wynik = [0] * len(portmonetka)
+    zostalo = kwota
+    for nominal in reversed(range(len(portmonetka))):
+        if zostalo <= 0:
+            break
+        if portmonetka[nominal] == 0:
+            continue
+        liczba_nominalow = min(zostalo // nominal, portmonetka[nominal])
+        print(zostalo, nominal, liczba_nominalow)
+        zostalo -= nominal * liczba_nominalow
+        portmonetka[nominal] = portmonetka[nominal] - liczba_nominalow
+        wynik[nominal] = liczba_nominalow
+    while zostalo > 0:
+        for nominal in reversed(range(len(portmonetka))):
+            if portmonetka[nominal] > 0:
+                zostalo = zostalo - nominal
+                portmonetka[nominal] -= 1
+                wynik[nominal] += 1
+        # całkiem pusta portmonetka
+        if portmonetka == ([0] * len(portmonetka)):
+            break
+    # print(suma)
+    return wynik, zostalo
+
+
+for i in range(10):
+    wynik, suma = rozmien([0, 2, 0, 0, 2, 2], i)
+    print(i, wynik, suma)
+
