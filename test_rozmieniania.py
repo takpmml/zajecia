0c810f4080c294972b799030d2068167321ada1b
marcin.mlotkowski <marcin.mlotkowski@testuj.pl>
2018-12-03 19:35:01 +0100

    dołożyłem jeszcze testy do rozmieniania

--- test_rozmieniania.py
+++ test_rozmieniania.py
@@ -6,3 +6,7 @@ for i in range(10):
     wynik, suma = rozmien([0, 2, 0, 0, 2, 2], i)
     print(i, wynik, suma)
 
+for i in range(20):
+    wynik, suma = rozmien([0, 2, 3, 6, 2, 2], i)
+    print(i, wynik, suma)
+
