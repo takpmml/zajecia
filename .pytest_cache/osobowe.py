8172637edfc9ecee83c0491400e1864618570278
marcin.mlotkowski <marcin.mlotkowski@testuj.pl>
2018-12-04 10:09:34 +0100

    przykład hierachii klas

--- .pytest_cache/osobowe.py
+++ .pytest_cache/osobowe.py
@@ -4,5 +4,33 @@ class Osoba:
         self.imie = imie
         self.nazwisko = nazwisko
 
+    def __str__(self):
+        return self.imie + " " + self.nazwisko
+
+class Pracownik(Osoba):
+
+    def __init__(self, imie, nazwisko, pensja):
+        Osoba.__init__(self, imie, nazwisko)
+        self.pensja = pensja
+
+    def wyplata(self):
+        return self.pensja * 1.2
+
+class Kierownik(Pracownik):
+    """
+    def __init__(self, imie, nazwisko, pensja):
+        Pracownik.__init__(self, imie, nazwisko, pensja)
+    """
+
+    def wyplata(self):
+        return super().wyplata() + 1200.0
 
 o = Osoba("Jan",  "Kowalski")
+print(o)
+
+p = Pracownik("Jan", "Nowak", 2250)
+
+print("{0} wypłata {1}".format(p, p.wyplata()))
+
+k = Kierownik("Anna", "", 5000)
+print(k.wyplata())
